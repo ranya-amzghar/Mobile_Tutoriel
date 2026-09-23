c65756e7c26a1925b876b53ead7619eeb9a60872
ranya-tech <amzghar.ranya.solicode@gmail.com>
2026-09-23 11:26:24 +0100

    Définir la communication (HTTP & API)

---
+++ Structurer l’application en composants/Définir_communication_(HTTP & API)/backend/categories.php
@@ -0,0 +1,11 @@
+<?php
+
+header('Content-Type: application/json');
+
+$categories = [
+    ["id" => 1, "nom" => "Développement Web"],
+    ["id" => 2, "nom" => "Design UI/UX"]
+];
+
+echo json_encode($categories);
+?>
